3e91452f951ecef8f4a61d99fa5575d00da5ba77
Marko Man <darkobas@gmail.com>
2019-08-15 23:17:47 +0200

    drm: msm: Allow DC dimming code to run
    Change-Id: Ibac6d5946dd68236ee4c076daae2f82c0f1bd383
    Signed-off-by: baalajimaestro <me@baalajimaestro.me>

--- drivers/gpu/drm/msm/sde/sde_crtc.c
+++ drivers/gpu/drm/msm/sde/sde_crtc.c
@@ -5782,19 +5782,7 @@ static int sde_crtc_onscreenfinger_atomic_check(struct sde_crtc_state *cstate,
             aod_index = i;
 	}
 
-	if (fppressed_index > 0 || fp_mode == 1) {
-		display->panel->dim_status = true;
-		cpu_input_boost_kick_max(500);
-		devfreq_boost_kick_max(DEVFREQ_MSM_LLCCBW_DDR, 500);
-		devfreq_boost_kick_max(DEVFREQ_MSM_CPU_LLCCBW, 500);
-		cstate->fingerprint_pressed = true;
-		return 0;
-	} else {
-		display->panel->dim_status = false;
-		cstate->fingerprint_pressed = false;
-		cstate->fingerprint_dim_layer = NULL;
-		return 0;
-	}
+	display->panel->dim_status = fp_index >= 0 && dim_mode != 0;
 
 	if(aod_index <0){
 		oneplus_aod_hid = 0;
@@ -5936,6 +5924,19 @@ static int sde_crtc_onscreenfinger_atomic_check(struct sde_crtc_state *cstate,
     }
 	if (fp_index < 0 && !dim_backlight) {
 		cstate->fingerprint_dim_layer = NULL;
+	}
+
+	if (fp_mode == 1) {
+		cpu_input_boost_kick_max(500);
+		devfreq_boost_kick_max(DEVFREQ_MSM_LLCCBW_DDR, 500);
+		devfreq_boost_kick_max(DEVFREQ_MSM_CPU_LLCCBW, 500);
+		display->panel->dim_status = true;
+		cstate->fingerprint_pressed = true;
+		return 0;
+	} else if (fp_mode == 0) {
+		display->panel->dim_status = false;
+		cstate->fingerprint_pressed = false;
+		return 0;
 	}
 
 	return 0;
