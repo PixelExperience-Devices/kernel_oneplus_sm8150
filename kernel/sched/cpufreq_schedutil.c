e453731bd40ba4c8d5196bfba8aefc8241b8c33d
Zachariah Kennedy <zkennedy87@gmail.com>
2019-10-29 23:17:42 +0000

    sched: Optimize for PELT
    Signed-off-by: Zachariah Kennedy <zkennedy87@gmail.com>
    Signed-off-by: baalajimaestro <me@baalajimaestro.me>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -180,7 +180,7 @@ static void sugov_track_cycles(struct sugov_policy *sg_policy,
 	u64 delta_ns, cycles;
 	u64 next_ws = sg_policy->last_ws + sched_ravg_window;
 
-	if (unlikely(!sysctl_sched_use_walt_cpu_util))
+	if (likely(!sysctl_sched_use_walt_cpu_util))
 		return;
 
 	upto = min(upto, next_ws);
@@ -199,7 +199,7 @@ static void sugov_calc_avg_cap(struct sugov_policy *sg_policy, u64 curr_ws,
 	u64 last_ws = sg_policy->last_ws;
 	unsigned int avg_freq;
 
-	if (unlikely(!sysctl_sched_use_walt_cpu_util))
+	if (likely(!sysctl_sched_use_walt_cpu_util))
 		return;
 
 	BUG_ON(curr_ws < last_ws);
@@ -253,7 +253,7 @@ static void sugov_update_commit(struct sugov_policy *sg_policy, u64 time,
 		}
 		cpufreq_stats_record_transition(policy, next_freq);
 	} else {
-		if (use_pelt())
+		if (likely(use_pelt()))
 			sg_policy->work_in_progress = true;
 		irq_work_queue(&sg_policy->irq_work);
 	}
@@ -397,7 +397,7 @@ static void sugov_walt_adjust(struct sugov_cpu *sg_cpu, unsigned long *util,
 	unsigned long cpu_util = sg_cpu->util;
 	bool is_hiload;
 
-	if (unlikely(!sysctl_sched_use_walt_cpu_util))
+	if (likely(!sysctl_sched_use_walt_cpu_util))
 		return;
 
 	is_hiload = (cpu_util >= mult_frac(sg_policy->avg_cap,
@@ -593,7 +593,7 @@ static void sugov_work(struct kthread_work *work)
 				CPUFREQ_RELATION_L);
 	mutex_unlock(&sg_policy->work_lock);
 
-	if (use_pelt())
+	if (likely(use_pelt()))
 		sg_policy->work_in_progress = false;
 }
 
