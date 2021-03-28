174d721297f95e2994df348ff3d22541c73e909d
baalajimaestro <me@baalajimaestro.me>
2021-03-28 05:51:09 +0000

    defconfig: Add cpusets and stune assist
    Signed-off-by: baalajimaestro <me@baalajimaestro.me>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -8205,7 +8205,6 @@ static int find_energy_efficient_cpu(struct sched_domain *sd,
 	u64 start_t = 0;
 	int next_cpu = -1, backup_cpu = -1;
 	int boosted = (schedtune_task_boost(p) > 0);
-	bool sync_boost = false;
 	bool about_to_idle = (cpu_rq(cpu)->nr_running < 2);
 
 	fbt_env.fastpath = 0;
